5e0f7fdc316aa0973f7be0e9a694c3550d076d65
Artem Smirnov <parov91@mail.ru>
2023-07-24 20:49:58 +0300

    added section product & slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function () {
   $('.burger').on('click', function () {
     $('.header__menu-list').toggleClass('header__menu-list--active');
-  })
+  });
+
+  $('.slider__list').slick({
+       infinite: true,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+       arrows: false,
+       autoplay: true
+  });
 })
